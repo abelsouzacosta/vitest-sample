da71dae2a4ce7a65ad3a78b13125881caf7e44ea
Abel Junior <abel.costa@webjump.com.br>
2023-02-02 14:45:12 -0300

    feat(math): adding array validations

--- src/math.js
+++ src/math.js
@@ -1,3 +1,22 @@
+function arrayIsNotEmpty(arr) {
+  if (arr.length === 0)
+    throw new Error("An non empty array should be provided");
+}
+
+function validateElements(arr) {
+  return arr.map(element => {
+    if (typeof element !== "number")
+      throw new Error("Invalid argument in the given array");
+  });
+}
+
+function validateArray(arr) {
+  if (typeof arr === 'undefined' || !Array.isArray(arr)) throw new Error("An array should be provided");
+  arrayIsNotEmpty(arr);
+  validateElements(arr);
+}
+
 export function add(numbers) {
+  validateArray(numbers);
   return numbers.reduce((accumulator, current) => accumulator + current, 0);
 }
--- src/tests/math.spec.js
+++ src/tests/math.spec.js
@@ -2,9 +2,47 @@ import { describe, it, expect } from 'vitest';
 import { add } from '../math';
 
 describe("add", () => {
-  it("should return the sunm of the numbers inside the given array", () => {
-    const numbers = [1, 2, 3]
-    const result = add(numbers);
-    expect(result).toBe(6);
-  });
+  describe("Success Cases", () => {
+    it("should return the sum of the numbers inside the given array", () => {
+      const numbers = [1, 2, 3]
+      const result = add(numbers);
+      expect(result).toBe(6);
+    });
+  })
+
+  describe("Fail Cases", () => {
+    it("should throw an error if an a invalid object is given as the parameter", () => {
+      const invalidArray = [1, "a", 2];
+      const resultFn = () => {
+        add(invalidArray)
+      }
+      expect(resultFn).toThrowError("Invalid argument in the given array");
+    });
+
+    it("should return an error if an a empty arrays is given", () => {
+      const empty = [];
+      const resultFn = () => {
+        add(empty);
+      };
+      expect(resultFn).toThrowError("An non empty array should be provided")
+    });
+
+    it("should return error if an argument is not given",  () => {
+      const resultFn = () => {
+        add();
+      }
+      expect(resultFn).toThrowError("An array should be provided");
+    });
+
+    it("should throw an error if an invalid argument is given", () => {
+      const invalidArgument = {
+        number1: 1,
+        number2: 2
+      };
+      const resultFn = () => {
+        add(invalidArgument);
+      };
+      expect(resultFn).toThrowError("An array should be provided");
+    })
+  })
 });
